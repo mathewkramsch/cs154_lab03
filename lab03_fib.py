da9977463a2323fac0efcb9fb125ab55349bc546
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-04-21 21:10:05 -0700

    cleaned up code, added comments

--- lab03_fib.py
+++ lab03_fib.py
@@ -4,37 +4,25 @@
 import pyrtl
 import random
 
-def getNthFibNum(a, b, n):
-    """
-        Precondition: a,b = 1st,2nd terms of the sequence
-        Postcondition: returns the nth term of the Fibonacci Sequence
-    """
-    if (n==0): return a
-    if (n==1): return b
-    return getNthFibNum(a,b,n-1) + getNthFibNum(a,b,n-2)
-
 # Inputs and Outputs
 A = pyrtl.Input(bitwidth=32,name='A')  # 1st starting terms of the fib sequence
 B = pyrtl.Input(bitwidth=32,name='B')  # 2nd starting term (instead of seq. starting w/ 0 & 1)
 result = pyrtl.Output(bitwidth=32,name='result')
 
 # updates the value of A and B for each cycle
-# for each cycle, result should be A + B 
 a_reg = pyrtl.Register(bitwidth=32,name='a_reg')
 b_reg = pyrtl.Register(bitwidth=32,name='b_reg')
 
-# Set Register Values (if val==0, then registers != been set yet)
+# Set Register Values (default register value is 0)
 with pyrtl.conditional_assignment:
-    with a_reg + b_reg == 0:
+    with a_reg + b_reg == 0:  # if regs = 0, then havent been set yet
         a_reg.next |= A
         b_reg.next |= B
     with a_reg + b_reg != 0:
         a_reg.next |= b_reg
         b_reg.next |= a_reg + b_reg
         
-result <<= b_reg
-
-
+result <<= b_regs  # result should be sum of a,b registers for each cycle
 
 # Simulate and Test
 sim_trace = pyrtl.SimulationTrace()
@@ -44,8 +32,6 @@ for cycle in range(15):
         'A': 1,
         'B': 1
     })
-
-#sim.step_multiple(sim_inputs)
 sim_trace.render_trace()
 
 exit(0)
